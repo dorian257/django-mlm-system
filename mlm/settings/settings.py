b40b8868249c49ec5071e40647b5a532b913905c
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2019-07-13 17:00:20 +0200

    Gmail settings

--- mlm/settings/settings.py
+++ mlm/settings/settings.py
@@ -157,7 +157,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "../media")
 
 INTERNAL_IPS = ["127.0.0.1"]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "moutardestest@gmail.com"
+EMAIL_HOST_PASSWORD = "olfi2019crack"
 
 MLM_GROUP_NAME = "Moutardes"
 MLM_GROUP_URL = "moutarde"
